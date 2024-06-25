56a12e48bef73cd93cb73658f06c7e833dbebe51
José Acerbi Almeida Neto <joseacerbialmeidaneto@hotmail.com>
2024-06-25 09:36:06 -0300

    :sparkles: feat: adicionando estrutura de scripts

--- src/scripts/verifyGraph.py
+++ src/scripts/verifyGraph.py
@@ -1,23 +1,31 @@
-import time
-from collections import defaultdict, deque
-
 class Graph:
+    
     def _init_(self, directed=False):
+        return
 
     def add_edge(self, u, v):
-            
+        return
+    
     def num_vertices(self):
-
+        return
+   
     def num_edges(self):
-
+        return
+    
     def is_connected(self):
-       
+        return
+    
     def is_bipartite(self):
-       
+        return
+    
     def is_eulerian(self):
+        return
         
     def is_hamiltonian(self):
-
+        return
+    
     def is_cyclic(self):
-
-    def is_planar(self):
+        return
+    
+    def is_planar(self):    
+        return 
---
+++ src/scripts/visualizeGraph.py
@@ -0,0 +1,28 @@
+class Graph:   
+    
+    def adjacency_matrix(self):
+        return
+    
+    def adjacency_list(self):
+        return
+   
+    def depth_first_tree(self):
+        return
+    
+    def breadth_first_tree(self):
+        return
+    
+    def minimum_spanning_tree(self):
+        return
+    
+    def topological_sort(self):
+        return
+    
+    def shortest_path(self, source, target):
+        return
+    
+    def max_flow(self, source, target):
+        return
+    
+    def transitive_closure(self):
+        return
