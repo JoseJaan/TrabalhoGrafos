9394510ebe91caaacde3725f68d4c7f016e4a8de
Gabriel Ferreira <gf-castro2012@hotmail.com>
2024-07-30 22:27:44 -0300

    feat: criando matriz e lista de adjacência

---
+++ ler_guardar.py
@@ -0,0 +1,40 @@
+with open("teste.txt",'r') as arquivo:
+    entrada = arquivo.readlines()
+
+quantidade_vertices = int(entrada[0][0])
+
+is_direcionado = bool()
+if(entrada[1] == "nao_direcionado"):
+    is_direcionado = False
+if(entrada[0] == "direcionado"):
+    is_direcionado = True
+
+
+vertices = [[] for _ in range(quantidade_vertices+1)]
+
+arestas = entrada[2:]
+
+for aresta in arestas:
+    ligacao_v1 = int(aresta[2])
+    ligacao_v2 = int(aresta[4])
+    vertices[ligacao_v1].append(ligacao_v2)
+    vertices[ligacao_v2].append(ligacao_v1)
+
+
+matriz_adjacencia = [[] for _ in range(quantidade_vertices+1)]
+
+for linha in matriz_adjacencia:
+    for x in range(quantidade_vertices+1):
+        linha.append(0)
+
+
+if(not is_direcionado):
+    for aresta in arestas:
+        ligacao_v1 = int(aresta[2])
+        ligacao_v2 = int(aresta[4])
+        peso = int(aresta[6])
+        matriz_adjacencia[ligacao_v1][ligacao_v2] = peso
+        matriz_adjacencia[ligacao_v2][ligacao_v1] = peso
+
+
+print(matriz_adjacencia)
